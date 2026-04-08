54526419cd9e3bfdaf5b5301ff958232c5e97ba7
jmato <jmato@era.aero>
2026-04-09 00:23:05 +0200

    Project setup

---
+++ src/main/java/org/example/config_change_tracker/ConfigChangeTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.example.config_change_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigChangeTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigChangeTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/config_change_tracker/ConfigChangeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.config_change_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigChangeTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
